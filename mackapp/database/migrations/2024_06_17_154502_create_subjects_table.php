7057870389e8c7f7ab207c7a156dea3db20cc0b5
CSsangria <160290387+CSsangria@users.noreply.github.com>
2024-06-19 16:09:29 +0800

    hello23
    heyy23

---
+++ mackapp/database/migrations/2024_06_17_154502_create_subjects_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubjectsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('subjects', function (Blueprint $table) {
+            $table->id();
+            $table->string('course');
+            $table->string('year');
+            $table->string('subject_name');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('subjects');
+    }
+}
+
+///4444
