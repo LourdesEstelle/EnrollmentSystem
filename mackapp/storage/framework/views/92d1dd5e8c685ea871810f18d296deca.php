01a492f766773c1650ec433418801c07f73bcc3a
CSsangria <160290387+CSsangria@users.noreply.github.com>
2024-06-25 23:30:33 +0800

    yee
    yeah

--- mackapp/storage/framework/views/92d1dd5e8c685ea871810f18d296deca.php
+++
@@ -1,5 +0,0 @@
-<?php $__env->startSection('title', __('Not Found')); ?>
-<?php $__env->startSection('code', '404'); ?>
-<?php $__env->startSection('message', __('Not Found')); ?>
-
-<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\selap\OneDrive\Desktop\EnrollmentSystem-Overnight-Life - Copy - Copy (3)\mackapp\vendor\laravel\framework\src\Illuminate\Foundation\Exceptions/views/404.blade.php ENDPATH**/ ?>
